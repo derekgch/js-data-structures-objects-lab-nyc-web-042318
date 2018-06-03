f56f9b04221e55f2b18c05412bf4d89fe6f4d7b6
Derek Chen <derek.g.chen@gmail.com>
2018-06-03 21:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 };
-const kew = updateDriverWithKeyAndValue(driver, 'address', '11 Broadway');
-const aww = driver;
-console.log(aww);
-console.log(kew);
+
 
 function deleteFromDriverByKey() {
 
