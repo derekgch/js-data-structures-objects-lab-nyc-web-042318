bbbd784313b3afd97f7b699626895513492ae009
Derek Chen <derek.g.chen@gmail.com>
2018-06-03 22:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   const newObj = driver;
-  delete newObj.key;
+  delete newObj[key];
   return newObj;
 };
 
