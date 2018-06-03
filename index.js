038fa4a9a82398af4a4111edd372de9c9fb75e60
Derek Chen <derek.g.chen@gmail.com>
2018-06-03 21:24:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Write your solution in this file!
 driver = {};
+
+function updateDriverWithKeyAndValue() {
+  
+}
 function deleteFromDriverByKey() {
 
 }
-
+function updateDriverWithKeyAndValue() {
+  
+}
 function destructivelyUpdateDriverWithKeyAndValue() {
 
 }
